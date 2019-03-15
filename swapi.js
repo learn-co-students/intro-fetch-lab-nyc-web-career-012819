edd69e7fcb7ae89bd814475c427488d7b80c1ca1
Elizabeth Kosowski <elizabethkosowski@gmail.com>
2019-03-15 09:04:00 -0400

    Done.

--- swapi.js
+++ swapi.js
@@ -1,1 +1,13 @@
 // Write your swapi code in this file!
+
+// fetch('https://swapi.co/api/films/1/')
+// .then(res => res.json())
+// .then(json => console.log(json));
+//
+// let promiseData = fetch('https://swapi.co/api/films/1/');
+//
+// promiseData.then(response => reponse.json());
+//
+// promiseData
+// .then(response => response.json())
+// .then(json => console.log(json))
